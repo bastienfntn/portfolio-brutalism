3d09631744447ea065ac7329c406ce22f90ea3e6
Bastien Fontaine <b.fontaine@thecodingmachine.com>
2024-02-09 21:29:53 +0100

    feat(app): add first big title to the page, add font and color in tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css'
 
 function App() {
 
   return (
-    <h1 className="font-bold"> Welcome to my app !</h1>
+    <div className="flex w-screen h-screen bg-background overflow-hidden">
+      <div className="grid grid-rows-3 m-8 pb-8">
+        <div className="flex w-full h-full items-end">
+          <p className="bottom-0 left-0 uppercase font-chivo text-sm">Hi, I am</p>
+          <span className="uppercase font-chivo text-bigName leading-[180px]">BAST</span>
+        </div>
+        <div className="flex w-full h-full items-end">
+          <span className="uppercase font-chivo text-bigName leading-[180px]">IEN.</span>
+          <p className="bottom-0 left-0 uppercase font-chivo text-sm">I am</p>
+        </div>
+        <div className="flex flex-col w-full h-full justify-end">
+          <span className="uppercase font-chivo text-[128px] leading-[110px]">web</span>
+          <span className="uppercase font-chivo text-[128px] leading-[110px]">developer</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}", ".index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        chivo: ["Chivo", "sans-serif"],
+      },
+      colors: {
+        background: '#F5F3F5',
+      },
+      fontSize: {
+        'bigName': '16rem',
+      }
+    },
   },
   plugins: [],
 }
