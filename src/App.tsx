446d6d994e4bd7dce7a1c1ace1d94a075f8156cd
Bastien Fontaine <b.fontaine@thecodingmachine.com>
2024-02-14 21:16:52 +0100

    feat(app): add transitions on links

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,37 @@
 function App() {
 
   return (
-    <div className="flex w-screen h-screen bg-background overflow-hidden">
-      <div className="grid grid-rows-3 m-8 pb-8">
+    <div className="flex w-screen h-screen bg-background overflow-hidden items-center">
+      <div className="grid w-full h-full grid-rows-3 m-8 pb-16 pt-14">
         <div className="flex w-full h-full items-end">
-          <p className="bottom-0 left-0 uppercase font-chivo text-sm">Hi, I am</p>
+          <span className="bottom-0 left-0 uppercase font-chivo text-sm">Hi, I am</span>
           <span className="uppercase font-chivo text-bigName leading-[180px]">BAST</span>
         </div>
         <div className="flex w-full h-full items-end">
           <span className="uppercase font-chivo text-bigName leading-[180px]">IEN.</span>
-          <p className="bottom-0 left-0 uppercase font-chivo text-sm">I am</p>
+          <span className="bottom-0 left-0 uppercase font-chivo text-sm">I am</span>
         </div>
         <div className="flex flex-col w-full h-full justify-end">
           <span className="uppercase font-chivo text-[128px] leading-[110px]">web</span>
           <span className="uppercase font-chivo text-[128px] leading-[110px]">developer</span>
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center ms-20">
-        <span className="uppercase font-chivo text-[64px] leading-[70px]">Projects</span>
-        <div className="w-full h-1.5 bg-black"></div>
-        <span className="uppercase font-chivo text-[64px] leading-[70px]">Skills</span>
-        <div className="w-full h-1.5 bg-black"></div>
-        <span className="uppercase font-chivo text-[64px] leading-[70px]">About me</span>
-        <div className="w-full h-1.5 bg-black"></div>
+      <div className="w-full h-80 flex flex-col justify-center ms-20">
+        <div className="relative group w-full h-20">
+          <div className="absolute bottom-0 group-hover:h-20 w-full h-1.5 bg-black transition-all duration-300"/>
+          <span
+            className="absolute bottom-0 px-1 uppercase font-chivo text-[64px] leading-[70px] group-hover:text-white transition-all duration-300">Projects</span>
+        </div>
+        <div className="relative group w-full h-20">
+          <div className="absolute bottom-0 group-hover:h-20 w-full h-1.5 bg-black transition-all duration-300"/>
+          <span
+            className="absolute bottom-0 px-1 uppercase font-chivo text-[64px] leading-[70px] group-hover:text-white transition-all duration-300">Skills</span>
+        </div>
+        <div className="relative group w-full h-20">
+          <div className="absolute bottom-0 group-hover:h-20 w-full h-1.5 bg-black transition-all duration-300"/>
+          <span
+            className="absolute bottom-0 px-1 uppercase font-chivo text-[64px] leading-[70px] group-hover:text-white transition-all duration-300">About Me</span>
+        </div>
       </div>
     </div>
   )
