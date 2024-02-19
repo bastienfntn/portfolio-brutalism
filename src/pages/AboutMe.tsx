9231ad5a0f4947e15392c848946a2ecb9eb48b81
Bastien Fontaine <b.fontaine@thecodingmachine.com>
2024-02-19 22:36:37 +0100

    feat(app): add 3D animation

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -2,10 +2,13 @@ import * as THREE from 'three';
 import { useEffect, useRef } from "react";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 
 function AboutMe() {
   const container = useRef<HTMLDivElement>(null);
   useEffect(() => {
+    const clock = new THREE.Clock();
+    let mixer: THREE.AnimationMixer;
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('#F5F3F5');
 
@@ -14,14 +17,26 @@ function AboutMe() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(2);
     container.current?.appendChild(renderer.domElement);
-    camera.position.z = 2;
+    camera.position.z = 2.5;
 
     const light = new THREE.AmbientLight('#ffffff');
     scene.add(light);
 
     new GLTFLoader().load('src/assets/my_avatar.glb', async function (gltf) {
-      gltf.scene.position.y = -1;
-      scene.add(gltf.scene);
+      new FBXLoader().load('src/assets/bow2.fbx', function (fbx) {
+        const model = gltf.scene
+        gltf.scene.position.y = -1;
+
+        mixer = new THREE.AnimationMixer(model);
+        const action = mixer.clipAction(fbx.animations[0]);
+        action.play();
+
+        scene.add(model);
+
+
+      }, undefined, function (error) {
+        console.error(error);
+      });
     }, undefined, function (error) {
       console.error(error);
     });
@@ -30,9 +45,11 @@ function AboutMe() {
     controls.enableDamping = true;
     controls.enablePan = false;
     controls.enableZoom = false;
-    controls.autoRotate = true;
+    controls.autoRotate = false;
 
     const loop = () => {
+      const delta = clock.getDelta();
+      mixer?.update(delta);
       controls.update();
       renderer.render(scene, camera);
       window.requestAnimationFrame(loop);
